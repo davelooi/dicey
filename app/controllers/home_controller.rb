2752c81bd6a67d967ebc7498e6f1182267b91fbb
David Looi <david@cultureamp.com>
2018-06-24 09:43:29 +1000

    Render helpful link instead of hello world in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
-    render json: { hello: :world }
+    render json: how_to
+  end
+
+  private
+
+  def how_to
+    {
+      'rollADice': rolls_url,
+      'roll2d16Dice': rolls_url(n: 2, d: 20),
+    }
   end
 end
